ee31ab3142c2bd7e88f998780384630b26c7677e
Roguer1993 <99084028+Roguer1993@users.noreply.github.com>
2022-11-22 23:36:43 -0500

    Add files via upload

---
+++ desafio/js/efecto.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+   // sr.reveal('nombre de la clase')
+
+sr.reveal('.conten_principal', {
+        duration: 4000,
+        origin: 'down',
+        distance: '-100px'
+});
+
+sr.reveal('.conten_secundario', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-100px'
+});
+
+sr.reveal('.principal', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
